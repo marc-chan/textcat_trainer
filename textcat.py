9c09c2c0882ea2afc5470c1e7a8ea99f88bec361
Marc <marc.chan89@gmail.com>
2020-05-20 16:43:28 +0800

    added model binary

--- textcat.py
+++ textcat.py
@@ -1,3 +1,4 @@
+import os
 import spacy
 import numpy as np
 from spacy.tokens import Token, Doc, Span, DocBin
@@ -5,6 +6,7 @@ from spacy.lexeme import Lexeme
 from spacy.vocab import Vocab
 import random
 from spacy.util import minibatch, compounding 
+from sklearn.model_selection import train_test_split
 
 class BERTClassifier:
     def __init__(self, labels=None, processor=None):
@@ -24,19 +26,21 @@ class BERTClassifier:
         L2 = 0.0,
         alpha=1e-3,
         architecture="softmax_last_hidden"):
-
-        ## Preprocess training data
-        self.labels = set(y)
-        data = self.format_data(X, y=y)
-        train_data, val_data = train_val_split(data, val_pct)
         
+        ## Preprocess Data for Training
+        self.labels = set(y)
+        # data = self.format_data(X, y=y)
+        # train_data, val_data = train_val_split(data, val_pct)
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=val_pct, stratify=y)
+        train_data = self._format_train_data(X_train, y=y_train)
+
         ## Set up and validate training params
         valid_architectures = ["softmax_last_hidden", "softmax_class_vector", "softmax_tanh_class_vector", "softmax_pooler_output"]
         if architecture not in valid_architectures:
             raise ValueError(f"`architecture` must be one of {valid_architectures}")
         learn_rate = cyclic_triangular_rate(learn_rate/3, learn_rate*3, len(train_data)//batch_size*2)
         eval_window = max(100, (len(train_data) // batch_size) // 10)
-        
+
         ## Set up training pipeline
         if 'trf_textcat' not in self.processor.pipe_names:
             textcat = self.processor.create_pipe(
@@ -73,7 +77,7 @@ class BERTClassifier:
                     self.processor.update(texts, cats, sgd=optimizer, drop=dropout, losses=train_losses)
                     if (batch_counter % eval_window) == 0:
                         with self.processor.use_params(optimizer.averages):
-                            val_acc = self.evaluate_accuracy(val_data)
+                            val_acc = self.evaluate_accuracy(X_val, y_val)
                         val_tape.append((val_acc, epoch_counter, batch_counter))
                         print(f"Epoch {epoch_counter}, Batch {batch_counter} -- train_loss: {train_losses['trf_textcat']}, val_acc: {val_acc}")
                         best_score, best_epoch, best_batch = max(val_tape)
@@ -88,38 +92,33 @@ class BERTClassifier:
                             continue
         return
 
-    def predict_one(self, X, *args, **kwargs):
+    def predict(self, X, *args, **kwargs):
         if not self.processor or 'trf_textcat' not in self.processor.pipe_names:
             raise NotImplementedError(f'A trained model is required.')
 
+        if isinstance(X, str):
+            X = [X]
+
         if self.best_params:
             with self.processor.use_params(self.best_params):
-                return self.processor(X).cats
+                return [doc.cats for doc in self.processor.pipe(X)]
         else:
-            return self.processor(X).cats
-
-    def predict(self, X, *args, **kwargs):
-        if isinstance(X, str):
-            return [self.predict_one(X, *args, **kwargs)]
-        else:
-            return [self.predict_one(x, *args, **kwargs) for x in X]
+            return [doc.cats for doc in self.processor.pipe(X)]
 
     def evaluate_accuracy(self, X, y):
         '''Evaluates accuracy of model on provided data.
 
         Arguments:
-            X str -- Text to perform prediction on
-            y dict -- Dict containing the ground truth labels (e.g. {'positive':True, 'negative': False})
+            X List[str] -- Text to perform prediction on
+            y List[str] -- Ground truth labels
         '''
         correct = 0
-        for text, actual in zip(X, y):
-            predict, _ = max(self.processor(text).cats.items(), key=lambda item: item[1])
-            actual = actual['cats'] if 'cats' in actual #Enable direct passing of spaCy-style labels
-            actual, _ = max(actual['cats'].items(), key=lambda item: item[1])
+        for doc, actual in zip(self.processor.pipe(X),y):
+            predict, _ = max(doc.cats.items(), key=lambda item: item[1])
             correct += int(predict==actual)
-        return correct/len(data)
+        return correct/len(y)
 
-    def format_data(self, X, y=None):
+    def _format_train_data(self, X, y=None):
         if y:
             data = [(x, {"cats": {cat:(True if cat==y_ else False) for cat in self.labels}}) for x ,y_ in zip(X, y)]
         else:
@@ -131,18 +130,26 @@ class BERTClassifier:
         processor = spacy.load(base_mdl_name)
         return cls(labels=set(), processor=processor)
 
+    def _fix_meta(self, path):
+        meta_fp = os.path.join(path,'meta.json')
+        meta_json = json.load(open(meta_fp,'r'))
+        if 'trf_textcat' in meta_json['factories']:
+            meta_json['factories']['trf_textcat'] = 'trf_textcat'
+            json.dump(meta_json, open(meta_fp,'w'))
+
     def to_disk(self, path):
         if self.best_params:
-            with self.processor.use_params(best_params):
+            with self.processor.use_params(self.best_params):
                 self.processor.to_disk(path)
         else:
             self.processor.to_disk(path)
+        self._fix_meta(path)
         
     def from_disk(self, path):
         self.processor = spacy.load(path)
+        self.meta = self.processor.get_pipe('trf_textcat').cfg
+        self.labels = set(self.meta['labels'])
         self.best_params = None
-        self.categories = set()
-
     
 def cyclic_triangular_rate(min_lr, max_lr, period):
     it = 1
@@ -153,15 +160,6 @@ def cyclic_triangular_rate(min_lr, max_lr, period):
         relative = max(0, 1 - x)
         yield min_lr + (max_lr - min_lr) * relative
         it += 1
-
-def train_val_split(data, val_pct):
-    if val_pct >= 1 or val_pct <= 0:
-        raise ValueError("`val_pct` must be between 0.0 and 1.0")
-    val_idx = int(len(data)*val_pct)
-    random.shuffle(data)
-    val_data = data[0:val_idx]
-    train_data = data[val_idx:]
-    return train_data, val_data
 
 class SentimentClassifier:
     def __init__(self, label_map=None, model=None):
